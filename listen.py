f463ad268bfd44e8f9dc19cc253eb6aa2c027bca
Sleepy Fox <sleepyfox@gmail.com>
2014-09-13 14:18:39 +0100

    split code out into separate listen and publish files

---
+++ listen.py
@@ -0,0 +1,19 @@
+from pika import BlockingConnection, ConnectionParameters
+
+RABBIT_HOST = '54.76.117.95'
+RABBIT_PORT = 5672
+EXCHANGE = 'combo'
+ROUTING_KEY = '#'
+
+def on_consume(ch, method, properties, body):
+  print body
+
+conn = BlockingConnection(ConnectionParameters(host=RABBIT_HOST,
+                                               port=RABBIT_PORT))
+channel = conn.channel()
+channel.exchange_declare(exchange=EXCHANGE, type='topic')
+result = channel.queue_declare(exclusive=True)
+queue = result.method.queue
+channel.queue_bind(exchange=EXCHANGE, queue=queue, routing_key = ROUTING_KEY)
+channel.basic_consume(on_consume, queue=queue, no_ack=True)
+channel.start_consuming()
